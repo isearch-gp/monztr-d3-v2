3ab1ead28b64e60c6eb2c326a057125cf337ce5a
mkobar <mike@exlorev.com>
2022-01-19 16:53:06 -0500

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiService } from "./shared/api.service";
 import { HttpClientModule } from "@angular/common/http";
+
+import { ApiService } from "./shared/api.service";
 import { MapperComponent } from './components/mapper/mapper.component';
 import { TreeReportComponent } from './components/tree-report/tree-report.component';
 @NgModule({
